3f40514bd695e1af5bdc28b752c3c10e9ebd57c4
Harshit <harshit_cc@yahoo.com>
2024-07-23 21:32:56 +0800

    testing code scan

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
 app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "password",
+  database: "test",
+});
 
 const print = require("./controller");
 
@@ -12,6 +21,16 @@ const postUser = require("./controllerPost");
 app.get("/", print);
 app.get("/home", home1);
 app.post("/user", postUser);
+
+app.get("/user", (req, res) => {
+  const userId = req.query.id;
+  const query = `SELECT * FROM users WHERE id = ${userId}`;
+
+  connection.query(query, (error, results) => {
+    if (error) throw error;
+    res.send(results);
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening to port ${process.env.PORT}`);
